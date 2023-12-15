e31e43c4bd15b065930963b873fbc401ef4cde5d
Daniel Muelle <danimr1999@gmail.com>
2023-12-15 08:52:10 +0100

    Improving solution for day 7 2023

---
+++ 2023/day_06/best-solution.js
@@ -0,0 +1,17 @@
+const movements = ">>*<";
+
+function maxDistance(movements) {
+  let distance = 0;
+
+  let right = movements.match(/>/g)?.length ?? 0;
+  let left = movements.match(/</g)?.length ?? 0;
+
+  distance += right;
+  distance -= left;
+
+  let extra = movements.length - (right + left);
+
+  return Math.abs(distance) + extra;
+}
+
+console.log(maxDistance(movements));
