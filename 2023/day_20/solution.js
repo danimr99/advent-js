453c0a4ec6e2eed6af9e87e58120842d86bf1933
Daniel Muelle <danimr1999@gmail.com>
2023-12-21 00:17:04 +0100

    Solution for day 20 2023

---
+++ 2023/day_20/solution.js
@@ -0,0 +1,27 @@
+const input = [
+  [4, 5, 1],
+  [6, null, 3],
+  [8, null, 4],
+];
+
+function distributeGifts(weights) {
+  return weights.map((row, i) =>
+    row.map((_, j) => {
+      const current = weights[i][j];
+      const left = weights[i]?.[j - 1];
+      const right = weights[i]?.[j + 1];
+      const top = weights[i - 1]?.[j];
+      const bottom = weights[i + 1]?.[j];
+
+      const existingValues = [current, left, right, top, bottom].filter(
+        (x) => x != null
+      );
+
+      return Math.round(
+        existingValues.reduce((a, b) => a + b, 0) / existingValues.length
+      );
+    })
+  );
+}
+
+console.log(distributeGifts(input));
