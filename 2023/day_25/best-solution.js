6a1bb63ad64a4fd4176780733f8613ca15528225
Daniel Muelle <danimr1999@gmail.com>
2023-12-25 16:59:41 +0100

    Improving solution for day 25 2023

---
+++ 2023/day_25/best-solution.js
@@ -0,0 +1,23 @@
+function travelDistance(map) {
+  let len = map.indexOf("\n") + 1;
+  len ||= map.length;
+
+  const chars = [...map.matchAll("[1-9S]")];
+  chars.sort();
+
+  let moves = 0;
+  let i = (chars.at(-1).index / len) | 0;
+  let j = chars.at(-1).index % len;
+
+  for (const c of chars.slice(0, chars.length - 1)) {
+    let mi = (c.index / len) | 0;
+    let mj = c.index % len;
+
+    moves += Math.abs(mi - i) + Math.abs(mj - j);
+
+    i = mi;
+    j = mj;
+  }
+
+  return moves;
+}
