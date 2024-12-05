890ccea8e4be89802373a6dfa7cf94d356e55a88
Daniel Muelle <danimr1999@gmail.com>
2024-12-05 23:32:57 +0100

    Refactor solution for day 1 2024 Python

--- 2024/day_01/solution.py
+++ 2024/day_01/solution.py
@@ -1,15 +1,15 @@
 def prepare_gifts(gifts: list[int | float]) -> list[int | float]:
   # Filter non-numeric values
-  gifts = [g for g in gifts if isinstance(g, (int, float))]
+  gifts = [gift for gift in gifts if isinstance(gift, (int, float))]
   
   # Delete duplicates keeping the original order
   seen = set()
   unique_gifts = []
   
-  for g in gifts:
-    if g not in seen:
-      seen.add(g)
-      unique_gifts.append(g)
+  for gift in gifts:
+    if gift not in seen:
+      seen.add(gift)
+      unique_gifts.append(gift)
 
   # Sort the gifts in ascending order
   return sorted(unique_gifts)
