83184ca18ac80f3060627a89a2e930811fcbb968
Daniel Muelle <danimr1999@gmail.com>
2024-12-06 09:35:50 +0100

    Improving solution for day 6 2024

--- 2024/day_06/solution.js
+++ 2024/day_06/solution.js
@@ -1,25 +1,13 @@
 function inBox(box) {
-  const rows = box.length;
-  const cols = box[0].length;
-
-  // Check every interior cell
-  for (let i = 1; i < rows - 1; i++) {
-    for (let j = 1; j < cols - 1; j++) {
-      if (box[i][j] === "*") {
-        // If exists an '*', check that it is not in the border
-        if (
-          !box[0].includes("*") && // Not in the top row
-          !box[rows - 1].includes("*") && // Not in the bottom row
-          box[i][0] === "#" && // Not in the left border
-          box[i][cols - 1] === "#" // Not in the right border
-        ) {
-          return true;
-        }
-      }
+  for (let i = 1; i < box.length - 1; i++) {
+    if (
+      box[i].includes("*") &&
+      box[i].indexOf("*") > 0 &&
+      box[i].indexOf("*") < box[i].length - 1
+    ) {
+      return true;
     }
   }
-
-  // No valid '*' found
   return false;
 }
 
