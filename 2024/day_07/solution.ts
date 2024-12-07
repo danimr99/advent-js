c9836f5658ac3f1d937dbc591d3c1bf146b45cc5
Daniel Muelle <danimr1999@gmail.com>
2024-12-07 12:50:44 +0100

    Solution for day 7 2024 TypeScript

---
+++ 2024/day_07/solution.ts
@@ -0,0 +1,16 @@
+(() => {
+  function fixPackages(packages: string): string {
+    while (packages.includes("(")) {
+      packages = packages.replace(/\(([^()]*)\)/g, (_, p1) =>
+        p1.split("").reverse().join("")
+      );
+    }
+
+    return packages;
+  }
+
+  console.log(fixPackages("a(cb)de")); // ➞ "abcde"
+  console.log(fixPackages("a(bc(def)g)h")); // ➞ "agdefcbh"
+  console.log(fixPackages("abc(def(gh)i)jk")); // ➞ "abcighfedjk"
+  console.log(fixPackages("a(b(c))e")); // ➞ "acbe"
+})();
