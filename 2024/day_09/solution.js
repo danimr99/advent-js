822101db69383def332dedcfc0d2b785ee33c08b
Daniel Muelle <danimr1999@gmail.com>
2024-12-09 01:42:56 +0100

    Improving solution for day 9 2024

--- 2024/day_09/solution.js
+++ 2024/day_09/solution.js
@@ -1,20 +1,17 @@
 function moveTrain(board, mov) {
   const directions = { L: [0, -1], R: [0, 1], U: [-1, 0], D: [1, 0] };
   const [dx, dy] = directions[mov];
-  let [x, y] = board.reduce(
+  const [x, y] = board.reduce(
     (acc, row, i) => (row.includes("@") ? [i, row.indexOf("@")] : acc),
     []
   );
 
   const [newX, newY] = [x + dx, y + dy];
+
   if (newX < 0 || newX >= board.length || newY < 0 || newY >= board[0].length)
     return "crash";
 
-  const cell = board[newX][newY];
-  if (cell === "@" || cell === "o") return "crash";
-  if (cell === "*") return "eat";
-
-  return "none";
+  return { "@": "crash", o: "crash", "*": "eat" }[board[newX][newY]] || "none";
 }
 
 const board = ["·····", "*····", "@····", "o····", "o····"];
