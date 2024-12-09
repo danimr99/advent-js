00aa70fb3808c9ee0245c5f79f5659723f39a7ac
Daniel Muelle <danimr1999@gmail.com>
2024-12-09 01:19:19 +0100

    Improving solution for day 9 2024

--- 2024/day_09/solution.js
+++ 2024/day_09/solution.js
@@ -1,40 +1,18 @@
 function moveTrain(board, mov) {
   const directions = { L: [0, -1], R: [0, 1], U: [-1, 0], D: [1, 0] };
   const [dx, dy] = directions[mov];
-  let train = [];
-  let head;
+  let [x, y] = board.reduce(
+    (acc, row, i) => (row.includes("@") ? [i, row.indexOf("@")] : acc),
+    []
+  );
 
-  // Find the train's head and carriages
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      if (board[i][j] === "@") head = [i, j];
-      if (board[i][j] === "@" || board[i][j] === "o") train.push([i, j]);
-    }
-  }
-
-  const newHead = [head[0] + dx, head[1] + dy];
-
-  // Check for collisions with the board edges
-  if (
-    newHead[0] < 0 ||
-    newHead[0] >= board.length ||
-    newHead[1] < 0 ||
-    newHead[1] >= board[0].length
-  ) {
+  const [newX, newY] = [x + dx, y + dy];
+  if (newX < 0 || newX >= board.length || newY < 0 || newY >= board[0].length)
     return "crash";
-  }
 
-  // Check for collisions with itself
-  for (const [x, y] of train) {
-    if (newHead[0] === x && newHead[1] === y) {
-      return "crash";
-    }
-  }
-
-  // Check if it collects a magical fruit
-  if (board[newHead[0]][newHead[1]] === "*") {
-    return "eat";
-  }
+  const cell = board[newX][newY];
+  if (cell === "@" || cell === "o") return "crash";
+  if (cell === "*") return "eat";
 
   return "none";
 }
