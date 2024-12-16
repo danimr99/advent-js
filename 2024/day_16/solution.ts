b875da90768dcca4295064743ffb1b26c776d5e3
Daniel Muelle <danimr1999@gmail.com>
2024-12-17 00:23:54 +0100

    Solution for day 16 2024 TypeScript

---
+++ 2024/day_16/solution.ts
@@ -0,0 +1,20 @@
+(() => {
+  function removeSnow(s: string): string {
+    let stack: string[] = [];
+
+    for (let char of s) {
+      if (stack.length && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    }
+
+    return stack.join("");
+  }
+
+  console.log(removeSnow("zxxzoz")); // -> "oz"
+  console.log(removeSnow("abcdd")); // -> "abc"
+  console.log(removeSnow("zzz")); // -> "z"
+  console.log(removeSnow("a")); // -> "a"
+})();
