3bb4965418f33aef8392fd627a5b7f1c59778243
Daniel Muelle <danimr1999@gmail.com>
2024-12-22 19:08:05 +0100

    Solution for day 22 2024 TypeScript

---
+++ 2024/day_22/solution.ts
@@ -0,0 +1,53 @@
+(() => {
+  function generateGiftSets(gifts: string[]): string[][] {
+    const result: string[][] = [];
+
+    // Helper function to generate combinations of size k
+    function generateCombinations(
+      start: number,
+      currentCombo: string[],
+      size: number
+    ): void {
+      if (currentCombo.length === size) {
+        result.push([...currentCombo]);
+        return;
+      }
+
+      for (let i = start; i < gifts.length; i++) {
+        currentCombo.push(gifts[i]);
+        generateCombinations(i + 1, currentCombo, size);
+        currentCombo.pop();
+      }
+    }
+
+    // Generate combinations of all sizes from 1 to n
+    for (let size = 1; size <= gifts.length; size++) {
+      generateCombinations(0, [], size);
+    }
+
+    return result;
+  }
+
+  console.log(generateGiftSets(["car", "doll", "puzzle"]));
+  // [
+  //   ['car'],
+  //   ['doll'],
+  //   ['puzzle'],
+  //   ['car', 'doll'],
+  //   ['car', 'puzzle'],
+  //   ['doll', 'puzzle'],
+  //   ['car', 'doll', 'puzzle']
+  // ]
+
+  console.log(generateGiftSets(["ball"]));
+  // [
+  //   ['ball']
+  // ]
+
+  console.log(generateGiftSets(["game", "pc"]));
+  // [
+  //   ['game'],
+  //   ['pc'],
+  //   ['game', 'pc']
+  // ]
+})();
