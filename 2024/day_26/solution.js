1f979b11e9796e2bca9d3ae5552c928f01589386
Daniel Muelle <danimr1999@gmail.com>
2024-12-28 01:51:54 +0100

    Improving solution for day 26 2024

--- 2024/day_26/solution.js
+++ 2024/day_26/solution.js
@@ -1,10 +1,26 @@
 function getCompleted(timeWorked, totalTime) {
-  const getSeconds = (timeStr) =>
-    timeStr.split(":").reduce((acc, time) => 60 * acc + +time);
+  function getSeconds(timeStr) {
+    let parts = [];
+    let current = "";
+    for (let char of timeStr) {
+      if (char === ":") {
+        parts.push(current);
+        current = "";
+      } else {
+        current += char;
+      }
+    }
+    parts.push(current);
+
+    let total = 0;
+    for (let part of parts) {
+      total = total * 60 + parseInt(part, 10);
+    }
+    return total;
+  }
 
   const workedSeconds = getSeconds(timeWorked);
   const totalSeconds = getSeconds(totalTime);
-
   const percentage = Math.round((workedSeconds / totalSeconds) * 100);
 
   return `${percentage}%`;
