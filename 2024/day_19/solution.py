d14f572cb838f83f20bc76b575bcc288f215a92a
Daniel Muelle <danimr1999@gmail.com>
2024-12-28 02:08:50 +0100

    Solution for day 19 2024 Python

--- 2024/day_19/solution.py
+++ 2024/day_19/solution.py
@@ -1,37 +1,51 @@
 def distribute_weight(weight: int) -> str:
-    # Box representations with proper spacing
-    box_representations = {
-        1: [" _ ", "|_|"],
-        2: [" ___ ", "|___|"],
-        5: [" _____ ", "|     |", "|_____|"],
-        10: [" _________ ", "|         |", "|_________|"]
+    boxes = {
+        1: [
+            " _ ",
+            "|_|",
+        ],
+        2: [
+            " ___ ",
+            "|___|",
+        ],
+        5: [
+            " _____ ",
+            "|     |",
+            "|_____|",
+        ],
+        10: [
+            " _________ ",
+            "|         |",
+            "|_________|",
+        ],
     }
-    
-    # Available weights in descending order
-    weights = [10, 5, 2, 1]
-    boxes = []
-    
-    # Get required boxes using greedy approach
-    for w in weights:
-        while weight >= w:
-            weight -= w
-            boxes.append(list(box_representations[w]))
-    
-    # Reverse to get ascending order (smaller boxes on top)
-    boxes.reverse()
-    
-    if not boxes:
-        return ""
-        
-    # Start with first box
-    stack = "\n".join(boxes[0])
-    
-    # Stack remaining boxes
-    for i in range(1, len(boxes)):
-        box = boxes[i]
-        # Remove overlap with previous box
-        box[0] = box[0][len(boxes[i-1][0]):]
-        # Join lines and trim right whitespace
-        stack = f"{stack[:-1]}{[line.rstrip() for line in box]}"
-    
-    return stack
+
+    sorted_weights = sorted(boxes.keys(), reverse=True)
+
+    output = []
+
+    while weight > 0:
+        w = None
+        for x in sorted_weights:
+            if x <= weight:
+                w = x
+                break
+
+        weight -= w
+
+        if not output:
+            output = boxes[w]
+        else:
+            box = boxes[w]
+
+            new_box_last_line = box[-1]
+
+            output_first_line = output[0]
+            slice_start = len(new_box_last_line)
+            output_slice = output_first_line[slice_start:].strip()
+
+            line = new_box_last_line + output_slice
+
+            output = box[:-1] + [line] + output[1:]
+
+    return "\n".join(output)
