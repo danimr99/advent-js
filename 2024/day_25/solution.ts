98c9d4afc29f7a3a82c5a46e28bf4e45bc7f2511
Daniel Muelle <danimr1999@gmail.com>
2024-12-28 01:44:58 +0100

    Solution for day 25 2024 TypeScript

---
+++ 2024/day_25/solution.ts
@@ -0,0 +1,47 @@
+(() => {
+  function execute(code: string): number {
+    let counter = 0;
+    let pointer = 0;
+
+    while (pointer < code.length) {
+      const char = code[pointer];
+
+      switch (char) {
+        case "+":
+          counter++;
+          break;
+        case "-":
+          counter--;
+          break;
+        case "[":
+          if (counter === 0) {
+            pointer = code.indexOf("]", pointer);
+          }
+          break;
+        case "]":
+          if (counter !== 0) {
+            pointer = code.lastIndexOf("[", pointer);
+            continue;
+          }
+          break;
+        case "{":
+          if (counter === 0) {
+            pointer = code.indexOf("}", pointer);
+          }
+          break;
+      }
+      pointer++;
+    }
+
+    return counter;
+  }
+
+  console.log(execute("+++")); // 3
+  console.log(execute("+--")); // -1
+  console.log(execute(">+++[-]")); // 0
+  console.log(execute(">>>+{++}")); // 3
+  console.log(execute("+{[-]+}+")); // 2
+  console.log(execute("{+}{+}{+}")); // 0
+  console.log(execute("------[+]++")); // 2
+  console.log(execute("-[++{-}]+{++++}")); // 5
+})();
