709174f889e4196b92992d6fc8c12226954f269e
Daniel Muelle <danimr1999@gmail.com>
2024-12-28 01:53:58 +0100

    Fix solution for day 26 2024

--- 2024/day_26/solution.js
+++ 2024/day_26/solution.js
@@ -16,3 +16,10 @@ function getCompleted(timeWorked, totalTime) {
 
   return `${percentage}%`;
 }
+
+console.log(getCompleted("01:00:00", "03:00:00")); // 33%
+console.log(getCompleted("02:00:00", "04:00:00")); // 50%
+console.log(getCompleted("01:00:00", "01:00:00")); // 100%
+console.log(getCompleted("00:10:00", "01:00:00")); // 17%
+console.log(getCompleted("01:10:10", "03:30:30")); // 33%
+console.log(getCompleted("03:30:30", "05:50:50")); // 60%
