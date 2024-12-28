4c47af8dc60195c9e6139fdca4ec0d443a18b7d9
Daniel Muelle <danimr1999@gmail.com>
2024-12-28 01:19:45 +0100

    Solution for day 24 2024

---
+++ 2024/day_24/solution.js
@@ -0,0 +1,46 @@
+export function isTreesSynchronized(tree1, tree2) {
+  function mirror(t1, t2) {
+    if (!t1 && !t2) return true;
+    if (!t1 || !t2) return false;
+
+    return (
+      t1.value === t2.value &&
+      mirror(t1.left, t2.right) &&
+      mirror(t1.right, t2.left)
+    );
+  }
+
+  return [mirror(tree1, tree2), tree1?.value];
+}
+
+const tree1 = {
+  value: "🎄",
+  left: { value: "⭐" },
+  right: { value: "🎅" },
+};
+
+const tree2 = {
+  value: "🎄",
+  left: { value: "🎅" },
+  right: { value: "⭐" },
+};
+
+console.log(isTreesSynchronized(tree1, tree2)); // [true, '🎄']
+
+const tree3 = {
+  value: "🎄",
+  left: { value: "🎅" },
+  right: { value: "🎁" },
+};
+
+console.log(isTreesSynchronized(tree1, tree3)); // [false, '🎄']
+
+const tree4 = {
+  value: "🎄",
+  left: { value: "⭐" },
+  right: { value: "🎅" },
+};
+
+console.log(isTreesSynchronized(tree1, tree4)); // [false, '🎄']
+
+console.log(isTreesSynchronized({ value: "🎅" }, { value: "🧑‍🎄" })); // [false, '🎅']
