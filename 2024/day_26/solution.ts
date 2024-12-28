c0216e6f022c6c1c7e841b35852660f8316f0715
Daniel Muelle <danimr1999@gmail.com>
2024-12-28 01:57:52 +0100

    Solution for day 26 2024 TypeScript

---
+++ 2024/day_26/solution.ts
@@ -0,0 +1,27 @@
+(() => {
+  function getCompleted(timeWorked: string, totalTime: string): string {
+    function getSeconds(str: string): number {
+      const parts = str.split(":");
+      let total = 0;
+
+      for (let i = 0; i < parts.length; i++) {
+        total = total * 60 + parseInt(parts[i], 10);
+      }
+
+      return total;
+    }
+
+    const workedSeconds = getSeconds(timeWorked);
+    const totalSeconds = getSeconds(totalTime);
+    const percentage = Math.round((workedSeconds / totalSeconds) * 100);
+
+    return `${percentage}%`;
+  }
+
+  console.log(getCompleted("01:00:00", "03:00:00")); // 33%
+  console.log(getCompleted("02:00:00", "04:00:00")); // 50%
+  console.log(getCompleted("01:00:00", "01:00:00")); // 100%
+  console.log(getCompleted("00:10:00", "01:00:00")); // 17%
+  console.log(getCompleted("01:10:10", "03:30:30")); // 33%
+  console.log(getCompleted("03:30:30", "05:50:50")); // 60%
+})();
